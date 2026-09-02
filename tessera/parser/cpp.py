39d1e8734fe1119fa466923a46479c219885b126
gk2657 <gauravskuwar@gmail.com>
2026-09-01 21:01:15 -0400

    updated parser code

--- tessera/parser/cpp.py
+++ tessera/parser/cpp.py
@@ -8,9 +8,7 @@ from tree_sitter import Language, Parser
 from tessera.parser.common import parse_text, find_nodes_by_type
 from tessera.const import KERNELS_DIR
 
-
 SPECIAL_CASE_INCLUDES = {"l0_int_mul_kar.h", "l0_int_mul_sb.h"}
-
 
 def parse_tmpl_class_name(tmpl_decl):
     cls = find_nodes_by_type(tmpl_decl, "class_specifier")[0]
@@ -18,8 +16,44 @@ def parse_tmpl_class_name(tmpl_decl):
 
 
 def parse_tmpl_params(tmpl_decl):
-    params = find_nodes_by_type(tmpl_decl, "template_parameter_list")[0]
-    return [parse_text(node) for node in params.named_children]
+    parsed_params = []
+    all_params = find_nodes_by_type(tmpl_decl, "template_parameter_list")[0]
+
+    # These are params where the type is not a class/struct
+    params_decls = find_nodes_by_type(all_params, "parameter_declaration")
+    opt_params_decls = find_nodes_by_type(all_params, "optional_parameter_declaration") # params with default values
+    
+    for param_decl in params_decls + opt_params_decls:
+        param_name = parse_text(param_decl.child_by_field_name("declarator"))
+        param_type = parse_text(param_decl.child_by_field_name("type"))
+        default_value = param_decl.child_by_field_name("default_value")
+        parsed_params.append({
+            "name": param_name,
+            "type": param_type,
+            "default": parse_text(default_value) if default_value else None
+        })
+    
+    # These are params where the type is a class/struct
+    type_params_decls = find_nodes_by_type(all_params, "type_parameter_declaration")
+    for param_decl in type_params_decls:
+        param_name = parse_text(find_nodes_by_type(param_decl, "type_identifier")[0])
+        parsed_params.append({
+            "name": param_name,
+            "type": "class",
+            "default": None
+        })
+
+    opt_type_params_decls = find_nodes_by_type(all_params, "optional_type_parameter_declaration") # params with default values
+    for param_decl in opt_type_params_decls:
+        param_name = parse_text(param_decl.child_by_field_name("name"))
+        default_type = parse_text(param_decl.child_by_field_name("default_type"))
+        parsed_params.append({
+            "name": param_name,
+            "type": "class",
+            "default": parse_text(default_type)
+        })
+
+    return parsed_params
 
 
 def parse_func(func_node):
@@ -38,14 +72,25 @@ def parse_class_field_decl(field_decl):
     
     # Is a dependency field declaration
     if decl_type == "template_type":
-        tmpl_types = find_nodes_by_type(field_decl, "template_type")
-        args = find_nodes_by_type(field_decl, "template_argument_list")
+        tmpl_type_node = find_nodes_by_type(field_decl, "template_type")[0]
+        tmpl_arg_ls = find_nodes_by_type(tmpl_type_node, "template_argument_list")[0]
+        tmpl_params = []
+
+        for arg_node in tmpl_arg_ls.children:
+            if arg_node.type == "type_descriptor":
+                if arg_node.child_by_field_name("type").type == "dependent_type":
+                    node = find_nodes_by_type(arg_node, "qualified_identifier")[0]
+                    tmpl_params.append(parse_text(node))
+                else:
+                    tmpl_params.append(parse_text(arg_node))
+            elif arg_node.type == "binary_expression":
+                tmpl_params.append(parse_text(arg_node))
 
         return {
             "name": name,
             "decl_type": decl_type,
-            "tmpl_name": parse_text(tmpl_types[0].child_by_field_name("name")),
-            "tmpl_params": [parse_text(node) for node in args[0].named_children],
+            "tmpl_name": parse_text(tmpl_type_node.child_by_field_name("name")),
+            "tmpl_params": tmpl_params,
             "declaration": declaration,
         }
 
@@ -77,6 +122,10 @@ def parse_header(header):
     parser = Parser(Language(tree_sitter_cpp.language()))
     root_node = parser.parse(Path(header).read_bytes()).root_node
     tmpl_decls = find_nodes_by_type(root_node, "template_declaration")
+
+    # each header should only contain one template class for now
+    assert len(tmpl_decls) <= 1, f"{header}: one template class per header"
+
     impl_spec = {
         "include_paths": parse_include(root_node),
     }
@@ -141,8 +190,9 @@ def parse_kernel_impl(kernel_impl, memo={}):
 if __name__ == "__main__":
     from pprint import pprint
 
-    header = "kernels/l0/l0_int_mul/impl/l0_int_mul_impl.h"
+    # header = "kernels/l0/l0_int_mul/impl/l0_int_mul_impl.h"
     # header = "kernels/l1/l1_mod_mul/impl/l1_mod_mul_impl.h"
+    header = "kernels/l2/l2_point_add_sw/impl/l2_point_add_sw_impl.h"
     # header = "kernels/l1/l1_mod_cmul/impl/l1_mod_cmul_impl.h"
     # parser = Parser(Language(tree_sitter_cpp.language()))
     # tree = parser.parse(Path(header).read_bytes())
